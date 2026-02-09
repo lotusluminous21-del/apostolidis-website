1b2cd1bc3e747d624d76de535775393e3a0ae8ea
lotusluminous21-del <lotus.luminous.21@gmail.com>
2026-02-09 17:30:34 +0200

    Secure Firebase credentials using environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBZENOOy_cFBhwdcPuybqj4nQ2ckY1cEZQ",
-    authDomain: "apostolidis-demo.firebaseapp.com",
-    projectId: "apostolidis-demo",
-    storageBucket: "apostolidis-demo.firebasestorage.app",
-    messagingSenderId: "407785895813",
-    appId: "1:407785895813:web:bf947a03359e64b89b36ec"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
